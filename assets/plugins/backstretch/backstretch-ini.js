5464d7a8ca736c5dee43b59054cb117313a5ed3e
CloudyDirk <Guenther_D@de.cenit-group.com>
2016-09-18 15:15:23 +0200

    Neues Image und Favicon hinzugefügt

--- assets/plugins/backstretch/backstretch-ini.js
+++ assets/plugins/backstretch/backstretch-ini.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $.getScript("assets/plugins/backstretch/jquery.backstretch.min.js", function() {
-    $(".fullscreen-static-image").backstretch("/assets/img/derdr-bg-wip1-xs.jpg");
+    $(".fullscreen-static-image").backstretch("/assets/img/derdr-bg-wip2-xs.jpg");
   });
 });
