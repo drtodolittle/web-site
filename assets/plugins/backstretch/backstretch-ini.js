538c79caea7bcd041e16f61ee186bd4bc71bf079
CloudyDirk <Guenther_D@de.cenit-group.com>
2016-09-18 13:13:26 +0200

    Neues Bild eingefügt

--- assets/plugins/backstretch/backstretch-ini.js
+++ assets/plugins/backstretch/backstretch-ini.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $.getScript("assets/plugins/backstretch/jquery.backstretch.min.js", function() {
-    $(".fullscreen-static-image").backstretch("/assets/img/fotolia_120438661.jpg");
+    $(".fullscreen-static-image").backstretch("/assets/img/derdr-bg-wip1-xs.jpg");
   });
 });
